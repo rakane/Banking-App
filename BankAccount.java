bd6038f249202b58e1b4310260be2cdd3a3107d4
rakane <rakane@syr.edu>
2018-10-16 00:52:36 -0400

    Adds adding account functionality

--- BankAccount.java
+++ BankAccount.java
@@ -1,13 +1,16 @@
+import java.util.Random;
+
 public class BankAccount {
 
+  Random gen = new Random();
   protected int accountNumber;
   protected int routingNumber;
   protected double accountBalance;
   private String type;
 
   public BankAccount(){
-    accountNumber = 0000000;
-    routingNumber = 0000000;
+    accountNumber = (int)(1000000 + gen.nextDouble() * 9000000);
+    routingNumber = (int)(1000000 + gen.nextDouble() * 9000000);
     accountBalance = 0;
   }
 
@@ -33,23 +36,27 @@ public class BankAccount {
     return type;
   }
 
-  public void deposit(double amount) {
+  public boolean deposit(double amount) {
     if(amount <= 0) {
-      System.out.println("Cannot deposit $" + amount);
+      System.out.println("Cannot deposit negative amount");
+      return false;
     }
     else{
       accountBalance += amount;
       System.out.println("Your account balance is now $" + accountBalance);
+      return true;
     }
   }
 
-  public void withdraw(int amount){
+  public boolean withdraw(double amount){
     if(amount > accountBalance){
       System.out.println("Cannot withdraw more than account balance");
+      return false;
     }
     else{
       accountBalance -= amount;
       System.out.println("Your account balance is now $" + accountBalance);
+      return true;
     }
   }
 
--- Main.java
+++ Main.java
@@ -73,7 +73,10 @@ public class Main {
       if(d.getUsers().get(i).getUserName().equalsIgnoreCase(u.getUserName())) {
         for(int j = 0; j < d.getUsers().get(i).getAccounts().size(); j++) {
           if(d.getUsers().get(i).getAccounts().get(j).getType().equals(input1)) {
-            d.getUsers().get(i).getAccounts().get(j).deposit(input2);
+            while(d.getUsers().get(i).getAccounts().get(j).deposit(input2) == false) {
+              System.out.print("Enter amount to deposit: ");
+              input2 = s.nextDouble();
+            }
           }
         }
       }
@@ -83,16 +86,63 @@ public class Main {
   }
 
   public static ArrayList<User> withdrawMenu(DataBase d, User u, Scanner s) {
+    String input1;
+    double input2;
+    System.out.print("Enter Checking or Saving to withdraw from respective account: ");
+    input1 = s.next();
+    System.out.print("Enter amount to withdraw: ");
+    input2 = s.nextDouble();
+
+    for(int i = 0; i < d.getUsers().size(); i++){
+      if(d.getUsers().get(i).getUserName().equalsIgnoreCase(u.getUserName())) {
+        for(int j = 0; j < d.getUsers().get(i).getAccounts().size(); j++) {
+          if(d.getUsers().get(i).getAccounts().get(j).getType().equals(input1)) {
+            while(d.getUsers().get(i).getAccounts().get(j).withdraw(input2) == false) {
+              System.out.print("Enter amount to withdraw: ");
+              input2 = s.nextDouble();
+            }
+          }
+        }
+      }
+    }
     s.close();
     return d.getUsers();
   }
 
   public static ArrayList<User> addAccountMenu(DataBase d, User u, Scanner s) {
+    String input1;
+    System.out.print("Enter Checking or Saving to add respective account: ");
+    input1 = s.next();
+
+    for(int i = 0; i < d.getUsers().size(); i++){
+      if(d.getUsers().get(i).getUserName().equalsIgnoreCase(u.getUserName())) {
+        for(int j = 0; j < d.getUsers().get(i).getAccounts().size(); j++) {
+          if(d.getUsers().get(i).getAccounts().get(j).getType().equals(input1)) {
+            System.out.println("Already have " + input1 + " account");
+            return d.getUsers();
+          }
+        }
+        if(input1.equals("Checking")) {
+          d.getUsers().get(i).addAccount(new CheckingsAccount());
+        }
+        else {
+          d.getUsers().get(i).addAccount(new SavingsAccount());
+        }
+      }
+    }
+
     s.close();
     return d.getUsers();
   }
 
   public static ArrayList<User> deleteAccountMenu(DataBase d, User u, Scanner s) {
+    String input1;
+    String input2;
+    System.out.print("Enter Checking or Saving to delete respective account: ");
+    input1 = s.next();
+    System.out.print("Enter account number to confirm: ");
+    input2 = s.next();
+
     s.close();
     return d.getUsers();
   }
